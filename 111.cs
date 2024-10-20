fb36b3cd7ed9c96e8b9795caa0912d83b2a565f6
smelikeaslan <aslansumeyramelike@gmail.com>
2024-10-20 23:18:15 +0300

    Add files via upload

---
+++ 111.cs
@@ -0,0 +1,31 @@
+﻿
+Console.Write("Birinci sayyıyı giriniz:");
+
+int birincisayi = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("İkinci sayıyı giriniz:");
+
+int ikincisayi = Convert.ToInt32(Console.ReadLine());
+
+
+if (birincisayi > ikincisayi)
+
+{
+    int fark = birincisayi - ikincisayi;
+
+    Console.Write("İki sayının farkı:" + fark);
+
+}
+else if (ikincisayi > birincisayi)
+{
+    int fark2 = ikincisayi - birincisayi;
+    Console.Write("iki sayının farkı:" + fark2);
+}
+
+else
+{
+    Console.Write("Sayıların farkı:0");
+
+}
+
+Console.ReadKey();
